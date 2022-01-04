f48e02decd87829f8026abc6acbd965321cb3bd1
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-04 23:03:32 +0200

    implemented interaction with API

--- image-finder/src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ image-finder/src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,20 +1,47 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { GalleryItem, GalleryItemImage } from "./ImageGalleryItem.styled";
+import getPictures from "../../utils/getPictures";
+import toastMsg from "../../utils/toastMsg";
 class ImageGalleryItem extends Component {
   static propTypes = {
     request: PropTypes.string.isRequired,
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("update request", this.props.request);
+  state = {
+    pictures: [],
+  };
+
+  componentDidUpdate(prevProps) {
+    const { request } = this.props;
+
+    if (prevProps.request !== request) {
+      getPictures(request).then((pic) => {
+        this.setState({ pictures: pic.hits });
+        toastMsg(`${pic.total} images found`, "info");
+      });
+      return;
+    }
+    console.log("no change request");
   }
 
   render() {
+    const { pictures } = this.state;
     return (
-      <GalleryItem>
-        <GalleryItemImage src="" alt="" />
-      </GalleryItem>
+      <>
+        {pictures.map(
+          ({ id, largeImageURL, webformatURL, webformatWidth, tags }) => (
+            <GalleryItem key={id}>
+              <GalleryItemImage
+                src={webformatURL}
+                alt={tags}
+                data-large={largeImageURL}
+                width={webformatWidth}
+              />
+            </GalleryItem>
+          )
+        )}
+      </>
     );
   }
 }
--- image-finder/src/utils/getPictures.js
+++ image-finder/src/utils/getPictures.js
@@ -5,10 +5,7 @@ const getPictures = async (request) => {
   const sQuery = request;
   const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${sQuery}&page=${pageNum}&per_page=${perPage}&key=${API_KEY}`;
 
-  const response = await fetch(URL, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
+  const response = await fetch(URL);
 
   return response.json();
 };
