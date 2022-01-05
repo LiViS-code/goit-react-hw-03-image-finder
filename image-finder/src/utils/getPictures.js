1714e50ac37b2bb9ee7d8b88908dd5bbd348ed64
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-05 13:45:56 +0200

    add page number

--- image-finder/src/utils/getPictures.js
+++ image-finder/src/utils/getPictures.js
@@ -1,7 +1,7 @@
-const getPictures = async (request) => {
+const getPictures = async (request, page = 1) => {
   const API_KEY = "24165340-8b0e7a7ee772979b6a936f118";
   const perPage = 12;
-  const pageNum = 1;
+  const pageNum = page;
   const sQuery = request;
   const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${sQuery}&page=${pageNum}&per_page=${perPage}&key=${API_KEY}`;
 
