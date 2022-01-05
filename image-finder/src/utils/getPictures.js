a66890130c55ca91eb9e2c27954e69447c66c9b9
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-05 23:58:26 +0200

    upd API func

--- image-finder/src/utils/getPictures.js
+++ image-finder/src/utils/getPictures.js
@@ -1,9 +1,7 @@
-const getPictures = async (request, page = 1) => {
+const getPictures = async (request, page = 1, perPage = 12) => {
   const API_KEY = "24165340-8b0e7a7ee772979b6a936f118";
-  const perPage = 12;
-  const pageNum = page;
-  const sQuery = request;
-  const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${sQuery}&page=${pageNum}&per_page=${perPage}&key=${API_KEY}`;
+
+  const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${request}&page=${page}&per_page=${perPage}&key=${API_KEY}`;
 
   const response = await fetch(URL);
 
